7d2bed34e4bbce9fc45bdcad0af6b98e87fba7e0
YousefHanbali <yousefhanbali5@gmail.com>
2023-05-13 12:35:34 +0300

    job_seekers

--- routers/signup.js
+++ routers/signup.js
@@ -33,4 +33,34 @@ function(req, res){
     });
 });
 
+Router.post('/api/signup/seeker',Validator,function(req, res, next){
+    // Validating 
+    if(!req.body.CompanyName){
+        return res.status(400).json({"Error": "There is insufficient information, please try again"});
+    }
+
+    // Inserting into Login Table
+    req.app.get('db').query('insert into log_in(username, email, password) values(?,?,?)',
+    [req.body.Username, req.body.Email, sha256(req.body.Password)],
+    (error) => {
+        if(error){
+            return res.status(500).json({"Error":err});
+        }else{
+            next();   
+        }
+    });
+},
+function(req, res){
+    // Inserting into employers table
+    req.app.get('db').query('INSERT INTO `job_seekers` (`Name`, `email`, `password`, `phone`, `Company_Name`) VALUES(?,?,?,?,?)',
+    [req.body.Name, req.body.Email, sha256(req.body.Password), req.body.Phone, req.body.CompanyName],
+    (error) => {
+        if(error){
+            return res.status(500).json({"Error":err});
+        }else{
+            return res.status(200).json({user: req.body}) 
+        }
+    });
+});
+
 module.exports = Router;
