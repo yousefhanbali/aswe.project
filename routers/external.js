f61fb679f7e57a5cb91b6f7f9e2da7788eeb2434
Diaa Arafat <arafatdiaa2@gmail.com>
2023-05-21 22:18:21 +0300

    Added the LinkedIn API

--- routers/external.js
+++ routers/external.js
@@ -1,22 +1,34 @@
 const express = require('express');
-const indeedJobsClient = require('../middlewares/jsonPlaceholder');
+const axios = require('axios');
 
 const router = express.Router();
 
+router.get('/', async (req, res) => {
+  const searchTerms = req.query.search_terms || 'python programmer';
+  const location = req.query.location || 'Chicago, IL';
+  const page = req.query.page || '1';
 
-router.get('/posts', async (req, res) => {
-  const { bs } = req.query;
-  console.log(bs);
+  const options = {
+    method: 'POST',
+    url: 'https://linkedin-jobs-search.p.rapidapi.com/',
+    headers: {
+      'content-type': 'application/json',
+      'X-RapidAPI-Key': 'bf07d1bcadmsh1a5ceb557fde6b6p10b8abjsn95b283f95708',
+      'X-RapidAPI-Host': 'linkedin-jobs-search.p.rapidapi.com'
+    },
+    data: {
+      search_terms: searchTerms,
+      location: location,
+      page: page
+    }
+  };
 
   try {
-    const posts = await indeedJobsClient.getPosts();
-
-    // Filter the posts based on the bs
-    const filteredPosts = posts.filter((post) => post.company.bs.includes(bs));
-
-    res.json(filteredPosts);
+    const response = await axios.request(options);
+    res.json(response.data);
   } catch (error) {
-    res.status(500).json({ error: 'Failed to retrieve posts' });
+    console.error(error);
+    res.status(500).send('An error occurred');
   }
 });
 
